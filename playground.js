145112251f6606bf1119a6ffcd049fe8d33417cd
pifragile <pg@solid-bit.com>
2024-06-17 16:12:02 +0200

    find missing documents

--- playground.js
+++ playground.js
@@ -7,7 +7,7 @@ import { parseEncointerBalance } from "@encointer/types";
 import util from "util";
 import BN from "bn.js";
 import { MongoClient } from "mongodb";
-import {findUnprocessedBlockNumbers, getLastProcessedBlockNumber, parseUnprocessedBlocks} from "./lib.js"
+import {findUnprocessedBlockNumbers, getLastProcessedBlockNumber} from "./lib.js"
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -22,6 +22,38 @@ export const ENCOINTER_RPC =
     process.env.ENCOINTER_RPC || "wss://kusama.api.encointer.org";
 
 
+export async function findUnprocessedBlockNumbers2(
+    blockNumber,
+    endBlockNumber
+) {
+    console.time('find all missing blocks')
+    const blocks = db.collection("blocks");
+    let unprocessedBlockNumbers = (await (
+        await blocks.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    nums: { $push: "$height" },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    missing_numbers: {
+                        $setDifference: [
+                            { $range: [blockNumber, endBlockNumber + 1] },
+                            "$nums",
+                        ],
+                    },
+                },
+            },
+        ])
+    ).toArray())[0].missing_numbers;
+
+    // for 5m documents, it takes about 2mins
+    console.timeEnd('find all missing blocks')
+    return unprocessedBlockNumbers;
+}
 async function main() {
     const wsProvider = new WsProvider(ENCOINTER_RPC);
     // Create our API with a default connection to the local node
@@ -33,11 +65,11 @@ async function main() {
 
 
     let lastProcessedBlockNumber = await getLastProcessedBlockNumber()
-    let unprocessedBlockNumbers = await findUnprocessedBlockNumbers(lastProcessedBlockNumber - 14400, lastProcessedBlockNumber)
+    let firstProcessedBlockNumber = (await db.collection("blocks").findOne({}, { sort: { height: 1 } })).height;
+    let unprocessedBlockNumbers = await findUnprocessedBlockNumbers2(firstProcessedBlockNumber, lastProcessedBlockNumber)
 
-    unprocessedBlockNumbers = await findUnprocessedBlockNumbers(508439, lastProcessedBlockNumber)
+    //unprocessedBlockNumbers = await findUnprocessedBlockNumbers(508439, lastProcessedBlockNumber)
 
-    console.log(unprocessedBlockNumbers.length)
     console.log(unprocessedBlockNumbers)
 
 
