d05863250336f885600f4176bb0b7d7cd986841d
Alain Brenzikofer <alain@integritee.network>
2024-09-28 21:31:11 +0200

    add system event indexing

--- lib.js
+++ lib.js
@@ -152,6 +152,21 @@ async function parseBlock(
 
             await insertIntoCollection("extrinsics", extrinsic);
         });
+        const systemEvents = allRecords
+          .filter(
+            ({ phase }) =>
+              phase.isFinalization || phase.isInitialization
+          )
+          .map((e) => e.toHuman());
+        systemEvents.forEach(async (e, eventIndex) => {
+            e.event.blockNumber = blockNumber;
+            e.event.blockHash = blockHash.toHuman();
+            e.event._id = `${blockNumber}-${eventIndex}`;
+            e.event.extrinsicId = null;
+            e.event.timestamp = block.timestamp;
+            delete e.event.index;
+            await insertIntoCollection("events", e.event);
+        })
         await insertIntoCollection("blocks", block);
     } catch (e) {
         throw e;
